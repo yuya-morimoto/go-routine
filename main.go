ad1e35912ccdceaee62aa45184bacb8d9fd87088
yuya-morimoto <gyu2243dev@gmail.com>
2023-03-25 20:26:50 +0900

    create: faninout

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	pkg.Pipeline()
+	pkg.FanInOut()
 }
---
+++ pkg/fanInOut.go
@@ -0,0 +1,96 @@
+package pkg
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func FanInOut() {
+	cores := runtime.NumCPU()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	outChs := make([]<-chan string, cores)
+	inData := generatorX(ctx, nums...)
+	for i := 0; i < cores; i++ {
+		outChs[i] = fanOut(ctx, inData, i+1)
+	}
+	var i int
+	flag := true
+	for v := range fanIn(ctx, outChs...) {
+		if i == 3 {
+			cancel()
+			flag = false
+		}
+		if flag {
+			fmt.Println(v)
+		}
+		i++
+	}
+	fmt.Println("finish")
+
+}
+
+func generatorX(ctx context.Context, nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- n:
+			}
+		}
+	}()
+	return out
+}
+
+func fanOut(ctx context.Context, in <-chan int, id int) <-chan string {
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		heavyWork := func(i int, id int) string {
+			time.Sleep(200 * time.Millisecond)
+			return fmt.Sprintf("result: %v (id:%v)", i*i, id)
+		}
+		for v := range in {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- heavyWork(v, id):
+			}
+		}
+	}()
+	return out
+}
+
+func fanIn(ctx context.Context, chs ...<-chan string) <-chan string {
+	var wg sync.WaitGroup
+	out := make(chan string)
+	multiplex := func(ch <-chan string) {
+		defer wg.Done()
+		for text := range ch {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- text:
+			}
+		}
+	}
+
+	wg.Add(len(chs))
+	for _, ch := range chs {
+		go multiplex(ch)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
